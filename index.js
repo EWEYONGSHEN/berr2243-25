0127f2904f984af5bb74c57c6e4d9e61c112d8c5
EWE YONG SHEN <b122320023@student.utem.edu.my>
2025-10-16 10:56:11 +0800

    Task 4: Query and Update Drivers

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 // EWE YONG SHEN B122320023
 const { MongoClient } = require('mongodb');
 
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvailable: true,
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvailable: false,
+        rating: 4.5
+    }
+];
+
+// show the data in the console
+console.log(drivers);
+
+// TODO: show all the drivers name in the console
+drivers.forEach((drivers) => console.log(drivers.name));
+
+// TODO: add additional driver to the drivers array
+const count = drivers.push(
+    {
+        name: "Alex",
+        vehicleType: "Hatchback",
+        isAvailable: true,
+        rating: 4.7
+    }
+);
+console.log(drivers);
+console.log(count);
+
 async function main() {
-    // Replace <connection-string> with your MongoDB URI
     const uri = "mongodb://localhost:27017"
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
-        console.log("Connected to MongoDB!");
-
         const db = client.db("testDB");
-        const collection = db.collection("users");
 
-        // Insert a document
-        await collection.insertOne({ name: "Alice", age: 25 });
-        console.log("Document inserted!");
+        const driversCollection = db.collection("drivers");
 
-        // Query the document
-        const result = await collection.findOne({ name: "Alice" });
-        console.log("Query result:", result);
-    }   catch (err) {
-        console.error("Error:", err);
+        drivers.forEach(async (driver) => {
+            const result = await driversCollection.insertOne(driver);
+            console.log(`New driver created with result: ${result}`);
+        });
+
+        const availableDrivers = await db.collection('drivers').find({
+            isAvailable: true,
+            rating: { $gte: 4.5 }
+        }).toArray();
+        console.log("Available drivers:", availableDrivers);
+
     }   finally {
         await client.close();
     }
